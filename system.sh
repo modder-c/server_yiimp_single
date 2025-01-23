068bf4b481b8d7744dd4642652bdb5be27fde4ce
modder-c <geldwallet2024@gmail.com>
2025-01-23 01:10:47 +0100

    Update system.sh

--- system.sh
+++ system.sh
@@ -61,9 +61,9 @@ echo -e "$GREEN Done...$COL_RESET"
 echo -e " Installing MariaDB Repository...$COL_RESET"
 hide_output sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 if [[ ("$DISTRO" == "18") ]]; then
-  sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu xenial main' >/dev/null 2>&1
+  sudo add-apt-repository  -y 'deb [arch=amd64,arm64,ppc64el] https://mirror.mariadb.org/repo/10.6/ubuntu bionic main' >/dev/null 2>&1
 else
-  sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu bionic main' >/dev/null 2>&1
+   sudo add-apt-repository -y 'deb [arch=amd64,arm64,ppc64el,s390x] https://mirror.mariadb.org/repo/10.6/ubuntu focal main' >/dev/null 2>&1
 fi
 echo -e "$GREEN Done...$COL_RESET"
 
@@ -139,12 +139,12 @@ fi
 hide_output sudo apt-get update
 
 if [[ ("$DISTRO" == "18") ]]; then
-apt_install php7.2-fpm php7.2-opcache php7.2-fpm php7.2 php7.2-common php7.2-gd \
-php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi \
+apt_install php7.3-fpm php7.3-opcache php7.3-fpm php7.3 php7.3-common php7.3-gd \
+php7.3-mysql php7.3-imap php7.3-cli php7.3-cgi \
 php-pear php-auth-sasl mcrypt imagemagick libruby \
-php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 \
-php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache \
-php-imagick php-gettext php7.2-zip php7.2-mbstring \
+php7.3-curl php7.3-intl php7.3-pspell php7.3-recode php7.3-sqlite3 \
+php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache \
+php-imagick php-gettext php7.3-zip php7.3-mbstring \
 fail2ban ntpdate python3 python3-dev python3-pip \
 curl git sudo coreutils pollinate unzip unattended-upgrades cron \
 pwgen libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev \
@@ -153,12 +153,12 @@ build-essential libtool autotools-dev automake pkg-config libevent-dev bsdmainut
 automake cmake gnupg2 ca-certificates lsb-release nginx certbot libsodium-dev \
 libnghttp2-dev librtmp-dev libssh2-1 libssh2-1-dev libldap2-dev libidn11-dev libpsl-dev libkrb5-dev
 else
-apt_install php7.2-fpm php7.2-opcache php7.2-fpm php7.2 php7.2-common php7.2-gd \
-php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi \
+apt_install php7.3-fpm php7.3-opcache php7.3-fpm php7.3 php7.3-common php7.3-gd \
+php7.3-mysql php7.3-imap php7.3-cli php7.3-cgi \
 php-pear php-auth-sasl mcrypt imagemagick libruby \
-php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 \
-php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache \
-php-imagick php-gettext php7.2-zip php7.2-mbstring \
+php7.3-curl php7.3-intl php7.3-pspell php7.3-recode php7.3-sqlite3 \
+php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache \
+php-imagick php-gettext php7.3-zip php7.3-mbstring \
 fail2ban ntpdate python3 python3-dev python3-pip \
 curl git sudo coreutils pollinate unzip unattended-upgrades cron \
 pwgen libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev \
